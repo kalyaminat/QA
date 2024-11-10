c51fab2a77b070eaad3285fb1f6a543f93efee8e
kalyaminat <kalyaminat@yandex.ru>
2024-11-10 18:29:59 +0300

    Урок 3

--- 03_lesson/adress.py
+++ 03_lesson/adress.py
@@ -1,10 +1,10 @@
 class Adress():
-    def __init__(self, индекс, город, улица, дом, квартира):
-        self.индекс = индекс
-        self.город = город
-        self.улица = улица
-        self.дом = дом
-        self.квартира = квартира
+    def __init__(self, index, city, street, house, appartment):
+        self.index = index
+        self.city = city
+        self.street = street
+        self.house = house
+        self.appartment = appartment
 
     def __str__(self):
-        return f"{self.индекс}, {self.город}, {self.улица}, {self.дом} - {self.квартира}"
+        return f"индекс{self.index}, город{self.city}, улица{self.street}, дом{self.house} - квартира{self.appartment}"
